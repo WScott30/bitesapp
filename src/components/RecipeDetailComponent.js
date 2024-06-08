f472b0b2fc4d5961db315ad7176ce6cffc435d0e
Sarah E <seatherly.prsvr@gmail.com>
2024-06-07 21:11:07 -0500

    working on recipe details

--- src/components/RecipeDetailComponent.js
+++ src/components/RecipeDetailComponent.js
@@ -1,21 +1,17 @@
 // src/components/RecipeDetailComponent.js
 import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
+
 import '../styles/RecipeDetailComponent.scss';
 import { useLocation } from 'react-router-dom';
 
 
 const RecipeDetailComponent = () => {
-    const { id } = useParams();
+    
     const location = useLocation();
     const recipeURI = location.state;
-    const dispatch = useDispatch();
+    
     const [recipe, setRecipe] = useState()
 
-    
-    const loading = useSelector(state => state.recipe.loading);
-    const error = useSelector(state => state.recipe.error);
 
     useEffect(() => {
         fetch(recipeURI)
@@ -44,6 +40,7 @@ const RecipeDetailComponent = () => {
                 <div className="recipe-detail">
                     <img src={recipe.images.REGULAR.url} alt={recipe.food_name} className="recipe-detail-image" />
                     <h1>{recipe.label}</h1>
+                    <h3 style={{fontWeight: 'lighter'}}>From: {recipe.source}</h3>
                     {recipe.totalTime !== 0 && <p>{getCookTime(recipe.totalTime)}</p>}
                     <p>{getCalories(recipe.calories)}</p>
                     <ul>
@@ -51,6 +48,7 @@ const RecipeDetailComponent = () => {
                             <li key={index}>{ingredient.text}</li>
                         ))}
                     </ul>
+                    <a href={recipe.url}>{recipe.url}</a>
                 </div>
             )}
         </div>
